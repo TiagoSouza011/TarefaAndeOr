de412cbaf9ef1e845f65d75d73b73699301d2930
EC2 Default User <ec2-user@ip-172-31-24-73.ec2.internal>
2023-05-06 17:43:56 +0000

    TarefaAndeOr

---
+++ ex5.py
@@ -0,0 +1,1 @@
+#5 -Escreva um código que verifique se uma pessoa é alfabetizada (sabe ler e escrever) e tem mais de 25 anos de idade.
---
+++ ex6.py
@@ -0,0 +1,1 @@
+#6 - Criar um programa que verifica se uma pessoa tem desconto em um produto, baseado na idade e sua categoria (estudante, aposentado, etc.) e no dia da semana. (Use quantas categorias desejar)
